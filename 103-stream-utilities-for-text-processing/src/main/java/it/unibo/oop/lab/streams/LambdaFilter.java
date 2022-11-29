aeace94fcba4ccf2dfdc752e4c970af22aec0f6a
AlessandroColi <alessandro.coli2@studio.unibo.it>
2022-11-29 17:26:57 +0100

    fine (riguarda COUNT_WORDS del 103) e implementa il test opzionale

--- 103-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 103-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -6,7 +6,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -33,12 +35,30 @@ import javax.swing.JTextArea;
 public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
 
     private enum Command {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        
+        COUNT_CHAR("Count the number of chars", e -> Integer.toString(e.length()) ),
+        
+        COUNT_LINES("Count the number of lines", e -> Long.toString( e.chars().filter( c -> c == '\n').count() + 1) ),
+        
+        COUNT_WORDS("Write the count for each word", e -> Arrays.stream(e.split(ANY_NON_WORD))
+        		.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        		.entrySet().stream()
+        		.map(c -> c.getKey() + " -> " + c.getValue())
+        		.collect(Collectors.joining("\n")) ),
+        
+        SORT("List all the words in alphabetical order", e -> Arrays.stream(e.split(ANY_NON_WORD))
+        		.sorted()
+        		.collect(Collectors.joining("\n"))  ),
+        
+    	TO_LOWER("Convert to lowercase", e -> e.toLowerCase() );
+    	
 
         private final String commandName;
         private final Function<String, String> fun;
